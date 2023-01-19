96e119ed1c5eed2d598d2e394a9941acad40a3e8
alaelbenna <elbenna.ala@outlook.com>
2023-01-19 16:55:09 +0100

    is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,70 +1,35 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *  reverseList - will reverse the singly linked
- * @temp: pointer to pointer of first node of listint_t list
- * Return: listint_t
- */
-listint_t *reverseList(listint_t *temp)
-{
-	listint_t *current = temp;
-	listint_t *prevNode = NULL, *nextNode = NULL;
+#include "lists.h"
 
-	while (current != NULL)
-	{
-		nextNode = current->next;
-		current->next = prevNode;
-		prevNode = current;
-		current = nextNode;
-	}
-	return (prevNode);
-}
 /**
- * is_palindrome - check for palindrome
- * @head: pointer to pointer of first node of listint_t list
- * Return: true or false
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int is_palindrome(listint_t **head)
+int main(void)
 {
-	listint_t *p, *q, *first_start, *second_start;
-		p = *head;
-		q = *head;
+    listint_t *head;
 
-		if (p->next == NULL)
-			return (1);
-		while (1)
-		{
-			p = p->next->next;
-			if (p == NULL)
-			{
-				second_start = q->next;
-				break;
-			}
-			if (p->next == NULL)
-			{
-				second_start = q->next->next;
-				break;
-			}
-			q = q->next;
-		}
-		q->next = NULL;
-		second_start = reverseList(second_start);
-		first_start = *head;
-		while (first_start != NULL && second_start != NULL)
-		{
-			if (first_start->n == second_start->n)
-			{
-				first_start = first_start->next;
-				second_start = second_start->next;
-				free(second_start);
-			}
-			else
-			{
-				return (0);
-			}
-		}
-		free(q);
-		free(p);
-		return (1);
+    head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
+    print_listint(head);
+
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
+    else
+        printf("Linked list is not a palindrome\n");
+
+    free_listint(head);
+
+    return (0);
 }
