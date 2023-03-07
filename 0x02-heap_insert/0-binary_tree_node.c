3084a83db1b8b957b193bb0aade2ad156bb74596
alaelbenna <elbenna.ala@outlook.com>
2023-03-07 19:02:43 +0100

    fix

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
+	heap_t *tmp = malloc(sizeof(binary_tree_t));
 
 	if (tmp == NULL)
 		return (NULL);
