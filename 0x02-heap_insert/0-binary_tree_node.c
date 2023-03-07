5aeb98056327e2a541f59157dcf197775e9947d2
alaelbenna <elbenna.ala@outlook.com>
2023-03-07 20:28:54 +0100

    create node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -14,9 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 
 	if (tmp == NULL)
+	{
 		return (NULL);
+		}
 	if (parent == NULL)
+	{
 		tmp->parent = NULL;
+		}
 		else{
 			tmp->parent=parent;
 		}
