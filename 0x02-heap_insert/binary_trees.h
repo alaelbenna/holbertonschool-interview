ab59470cd3a836d824c7696cf28f8a64add3999d
alaelbenna <elbenna.ala@outlook.com>
2023-03-07 20:26:16 +0100

    lib

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -22,5 +22,8 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *heap_insert_with_levels(heap_t **root, int value, int level, int max_level);
+
 
 #endif /* _BINARY_TREES_H_ */
