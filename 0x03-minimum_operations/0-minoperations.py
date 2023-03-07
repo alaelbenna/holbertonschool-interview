4c66ed0b8942e00a5926f3f4651eca02e1eff4d8
Ala <91365335+alaelbenna@users.noreply.github.com>
2023-03-07 21:05:21 +0100

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,22 +1,48 @@
 #!/usr/bin/python3
 """
-Minimum Operations
+Defines a method that calculates the fewest number of operations needed
+to result in exactly n copies of a character in a text file, whose
+only operations are Copy All and Paste
 """
 
 
 def minOperations(n):
     """
-    minOperations()
+    calculates the fewest number of operations needed
+    to result in exactly n copies of a character in a text file,
+    whose only operations are Copy All and Paste
+    parameters:
+        n [int]: number of copies of the character desired
+    returns:
+        the minimum number of operations needed to result in n characters
+        or 0 if n is impossible to achieve
     """
-
-    if n <= 0:
+    # minOperations will be the sum of all prime factors of n
+    # Check if given a valid positive int greater than 1
+    if type(n) is not int or n <= 1:
         return 0
-
-    count = []
-    i = 2
-    while n > 1:
-        while n % i == 0:
-            count.append(i)
-            n = n / i
-        i += 1
-    return sum(count)
+    # Start summation list to store divisors
+    summation = []
+    # start divisors at 2, first prime number
+    divisor = 2
+    # divide until no longer divisible or dividing by itself
+    while (n % divisor) is 0 and (n // divisor) is not 1:
+        # each time dividing, add divisor to list to sum later
+        summation.append(divisor)
+        # update the number to show the division
+        n = n // divisor
+    # update divisor to 3, next prime number
+    divisor = 3
+    # keep going until number is less than divisor
+    while n > divisor:
+        # keep completing divisions with new odd numbers
+        while (n % divisor) is 0 and (n // divisor) is not 1:
+            summation.append(divisor)
+            n = n // divisor
+        # move divisor to next odd number
+        # not-strictly prime, but later odd numbers are comprised of previous
+        divisor += 2
+    # add the number itself, last prime number
+    summation.append(n)
+    # return the sum of all prime factors
+    return sum(summation)
