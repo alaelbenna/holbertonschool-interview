bdcace49c15cf8affef56f247c8a8771dc5c62e7
alaelbenna <elbenna.ala@outlook.com>
2023-07-06 23:40:27 +0100

    fix this task

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Simple python code to determine the fewest
-number of coins needed to meet agiven amount " total ".
-"""
+"""module"""
 
 
 def makeChange(coins, total):
-    """
-    determine the fewest number of coins
-    needed to meet a given amount total
-    """
-    if total == 0:
+    """Given a pile of coins of different
+       values, determine the fewest number
+       of coins needed to meet a given amount total"""
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
         return 0
-
-    if total < 0 or len(coins) == 0:
-        return -1
-
-    min_coins = float('inf')
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        if coin <= total:
-            num_coins = makeChange(coins, total - coin)
-            if num_coins != -1:
-                min_coins = min(min_coins, num_coins + 1)
-
-    if min_coins == float('inf'):
-        return -1
-
-    return min_coins
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
