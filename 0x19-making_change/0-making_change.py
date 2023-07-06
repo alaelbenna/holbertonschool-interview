d35cbb5cc9126dbd5eaa45efb126167241fc8cae
alaelbenna <elbenna.ala@outlook.com>
2023-07-06 22:59:42 +0100

    fix code

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Simple python code to determine the fewest
+number of coins needed to meet agiven amount " total ".
+"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins
+    needed to meet a given amount total
+    """
+    if total == 0:
+        return 0
+
+    if total < 0 or len(coins) == 0:
+        return -1
+
+    min_coins = float('inf')
+
+    for coin in coins:
+        if coin <= total:
+            num_coins = makeChange(coins, total - coin)
+            if num_coins != -1:
+                min_coins = min(min_coins, num_coins + 1)
+
+    if min_coins == float('inf'):
+        return -1
+
+    return min_coins
